7c0d9e5d46f308f7911f03d5f8c920975b398b9f
shawn <shawnantonucci2018@gmail.com>
2019-07-02 19:20:11 -0700

    merge conflict

--- src/components/Dashboard/DisplayInfo.js
+++ src/components/Dashboard/DisplayInfo.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Statistic,  Segment} from "semantic-ui-react";
+import './DisplayInfo.css'
 
 const StatisticExampleValueShorthand = () => (
-    <Segment style={{marginBottom: "25px", width: "210px", marginLeft: "10px",}}>
+  <Segment style={{marginBottom: "25px", width: "210px", marginLeft: "10px", marginTop: '2%'}}>
         <h3 style={{textAlign: "center"}}>Project Data</h3>
         <Statistic.Group size="mini">
             <Statistic label="Projects" value="50" />
