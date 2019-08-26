3064503c19b650c006330575790a43740443f5cb
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-26 18:24:57 -0400

    Cleaned up file a bit

--- src/components/AddBuildWeeks.js
+++ src/components/AddBuildWeeks.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import {Form, Button, Input, FormField} from 'semantic-ui-react';
-import firebase, { db } from '../logic/firebase.js';
+import React, { useState } from 'react';
+import {Form, Button, Input } from 'semantic-ui-react';
+import { db } from '../logic/firebase.js';
 
 const AddBuildWeek = props => {
     const [buildWeekName, SetBuildWeekName] = useState("")
@@ -14,8 +14,6 @@ const AddBuildWeek = props => {
       const check = db.collection('build_weeks').doc(`${buildWeekName}`)
       const giveMe = await check.get()
       const exist = giveMe.data()
-      //console.log('check: ', check);
-      console.log('giveMe: ', giveMe.data());
     if (!exist) {
         db.collection('build_weeks').doc(`${buildWeekName}`).set({
             buildWeekName
@@ -33,12 +31,6 @@ const AddBuildWeek = props => {
         setError("That name is Already in the database!")
     }
 
-      //db.collection('build_weeks').add({buildWeekName}).then(ref => {
-          //buildWeekName.uid = ref._key.path.segment[1]
-          //ref.set({
-              //buildWeekName: {...ref.buildWeekName, uid: ref._key.path.segment[1]}
-          //})
-      //})
   };
   return (
       <Form onSubmit={e => addBuildWeek(e)}>
