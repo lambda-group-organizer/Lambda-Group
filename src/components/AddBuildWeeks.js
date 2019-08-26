dac4d97ae19430b38234a9024e68799706d5d471
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-26 13:56:16 -0400

    Adding BuildWeek to DB

--- src/components/AddBuildWeeks.js
+++ src/components/AddBuildWeeks.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Form, Button, Input, FormField} from 'semantic-ui-react';
+import firebase, { db } from '../logic/firebase.js';
 
 const AddBuildWeek = props => {
+    const [buildWeekName, SetBuildWeekName] = useState("")
+    const handleChange = (e) => {
+        SetBuildWeekName(e.target.value)
+    }
+
   const addBuildWeek = e => {
     e.preventDefault();
-      console.log('hello testing this@!');
+      db.collection('build_weeks').add({buildWeekName}).then(ref => {
+          console.log(ref)
+      })
   };
   return (
-    <>
       <Form onSubmit={e => addBuildWeek(e)}>
         <Form.Field>
-          <Input label="name" placeholder="build week ... ?" />
+          <Input label="name" value={buildWeekName} onChange={handleChange} placeholder="build week ... ?" />
         </Form.Field>
         <Button type="submit" color="green">
           test
         </Button>
       </Form>
-    </>
   );
 };
 
