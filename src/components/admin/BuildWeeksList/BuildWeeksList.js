aa539da385a24aa71e10d21aa21d03daa25d4669
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-27 08:43:20 -0700

    Displaying each build week complete

--- src/components/admin/BuildWeeksList/BuildWeeksList.js
+++ src/components/admin/BuildWeeksList/BuildWeeksList.js
@@ -1,19 +1,38 @@
-import React, {useState} from 'react'
-import {db}from '../../../logic/firebase';
+import React, {useState, useEffect} from 'react';
+import {db} from '../../../logic/firebase';
+import { Card, Button } from 'semantic-ui-react';
 
 const BuildWeeksList = props => {
-    const [ListOfBuildWeeks, setListOfBuildWeeks] = useState([])
+    const [listOfBuildWeeks, setListOfBuildWeeks] = useState([])
 
-    const fetchBuildWeeks = () => {
-
+    // needs to run each time a build week is created
+    const fetchBuildWeeks = async () => {
+      let buildWeeksCollection = await db.collection('build_weeks').get()
+      buildWeeksCollection.forEach(function(doc) {
+        setListOfBuildWeeks(prevSetOfBuildWeeks => {return [...prevSetOfBuildWeeks, `${doc.id}`]})
+        console.log(doc.id, '=>', doc.data())
+      })
     }
+    useEffect(() => {
+      fetchBuildWeeks()
+    }, []);
 
   return (
     <div>
-        <p>Build Week</p>
-        <p>Build Week</p>
-        <p>Build Week</p>
-        <p>Build Week</p>
+      <Card.Group>
+        {listOfBuildWeeks && listOfBuildWeeks.map(buildWeek => (
+          <Card key={`${buildWeek}`}>
+            <Card.Content>
+              <Card.Header>{buildWeek}</Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui buttons">
+                <Button basic color="red">Delete</Button>
+              </div>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
     </div>
   )
 }
