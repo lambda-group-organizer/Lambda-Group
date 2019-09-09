b7c9fad247e933aa9266191429cf54cbb8b0df9e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-09 14:26:12 -0400

    GOT CONTEXT WORKING AND LIVE UPDATING....
    finally a day freaking later woot woot!

--- src/components/students/StudentDashBoard/StudentBuildWeekView.js
+++ src/components/students/StudentDashBoard/StudentBuildWeekView.js
@@ -1,5 +1,5 @@
 // Modules
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import firebase, { db } from "../../../logic/firebase.js";
 import { Button, Card, Header, Form, Icon } from "semantic-ui-react";
 import { withRouter } from "react-router-dom";
@@ -10,7 +10,6 @@ import DashBoardHeader from "../../globalComponents/DashBoardHeader";
 import StudentProjectView from "./StudentProjectView/StudentProjectView";
 import LoginAnimation from "../../Auth/LoginAnimation";
 import "../../../Dashboard/Dashboard.css";
-// import fetchBuildWeekProjects from "../../../utils/fetchBuildWeekProjects";
 import ProjectViewModal from "../../globalComponents/ProjectViewModal/ProjectViewModal";
 
 const StudentBuildWeekView = props => {
@@ -20,84 +19,42 @@ const StudentBuildWeekView = props => {
     currentBuildWeekURL,
     setProjectRole,
     setCurrentSelectedProject,
-    setCurrentSelectedProjectUid
+    setCurrentSelectedProjectUid,
   } = useContext(UserContext);
 
-  const { projectsContext, setProjectsContext } = useContext(BuildWeekContext);
-  // const [projects, setProjects] = useState([]);
+  const { projectsContext, setProjectsContext, fetchBuildWeekProjects } = useContext(BuildWeekContext);
   const [filteredProjects, setFilteredProjects] = useState([]);
   // Local state
   const [projectModalData, setProjectModalData] = useState(null);
 
-  const signOut = () => {
-    firebase.auth().signOut();
-  };
-
-  const fetchBuildWeekProjects = async buildWeek => {
-    console.log("PROJECTS CONTEXT: ", projectsContext);
-    debugger;
-    let changeType = "";
-    let collection = await db
-      .collection("build_weeks")
-      .doc(`${buildWeek}`)
-      .collection("projects");
-    let observer = collection.onSnapshot(querySnapshot => {
-      let data = querySnapshot.docChanges().map(change => {
-        console.log(change.type);
-        changeType = change.type;
-        return change.doc.data();
-      });
-      console.log(data[0]);
-      if (changeType === "modified") {
-        console.log("MODIFIED IF");
-        console.log(projectsContext);
-        let newProjects = projectsContext.map(tempProject => {
-          console.log("TEMP PROJECT: ", tempProject);
-          if (data[0].uid === tempProject.uid) {
-            console.log(data);
-            return data[0];
-          } else {
-            return tempProject;
-          }
-        });
-        setProjectsContext(newProjects);
-      } else {
-        setProjectsContext(data);
-      }
-    });
-  };
 
   const fetchProjects = async () => {
     const { buildWeek } = props.match.params;
     fetchBuildWeekProjects(buildWeek, projectsContext);
-    console.log("PROJECTS UPDATED");
   };
 
   const getStudentRole = async () => {
     const userRef = db.collection("students").doc(user.uid);
     let data = await userRef.get();
     setProjectRole(data.data().buildWeeks[currentBuildWeekURL].projectRole);
-    console.log(data.data().buildWeeks[currentBuildWeekURL].project);
     setCurrentSelectedProject(
       data.data().buildWeeks[currentBuildWeekURL].project
     );
     setCurrentSelectedProjectUid(
       data.data().buildWeeks[currentBuildWeekURL].projectUid
     );
-    //console.log("data.data()", data.data())
   };
 
-  useEffect(() => {
-    fetchProjects();
+  useEffect((buildWeek, projectsContext) => {
+    fetchProjects(buildWeek, projectsContext);
     getStudentRole();
   }, []);
 
   useEffect(() => {
-    setFilteredProjects(projectsContext);
+      setFilteredProjects(projectsContext);
   }, [projectsContext]);
 
   function handleFuzzySearch(e) {
-    // console.log(projects);
     // Fuzzy search for students involved, title, description, productType(ios, web, etc)
     let searchResults = fuzzySearch(
       projectsContext,
