7feacc1dbccac81baa8858bbfe8a05fdc1dbb66f
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-09-09 17:17:37 -0700

    changed fuzzy search placeholder text

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,15 +1,15 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from "react";
 // import CurrentProjectContext from '../../context/CurrentProjectContext';
-import ProjectModal from './ProjectModal';
+import ProjectModal from "./ProjectModal";
 // import AddMinion from '../../components/Auth/AddMinion.js';
-import {Link} from 'react-router-dom';
-import firebase from '../logic/firebase';
-import {db} from '../logic/firebase';
-import {Button, Card, Header, Form, Icon} from 'semantic-ui-react';
-import DisplayInfo from './DisplayInfo';
-import './Dashboard.css';
-import Fuse from 'fuse.js';
-import LoginAnimation from '../components/Auth/LoginAnimation.js';
+import { Link } from "react-router-dom";
+import firebase from "../logic/firebase";
+import { db } from "../logic/firebase";
+import { Button, Card, Header, Form, Icon } from "semantic-ui-react";
+import DisplayInfo from "./DisplayInfo";
+import "./Dashboard.css";
+import Fuse from "fuse.js";
+import LoginAnimation from "../components/Auth/LoginAnimation.js";
 
 const Dashboard = () => {
   // const [currentProject, setCurrentProject] = useState(null);
@@ -17,7 +17,7 @@ const Dashboard = () => {
   const [tempProjects, setTempProjects] = useState([]);
   // const [projectName, setProjectName] = useState('');
   // const [projectDescription, setProjectDescription] = useState('');
-  const projectsRefFirebase = firebase.database().ref('projects');
+  const projectsRefFirebase = firebase.database().ref("projects");
   const [filteredProj, setFilteredProj] = useState([]);
 
   const refTo_projectsVariable = useRef();
@@ -51,9 +51,11 @@ const Dashboard = () => {
 
   useEffect(() => {
     const addProjectListener = async () => {
-      console.log('project listener is added');
-      let projectRef = await db.collection('projects');    // TODO: Make all of this function async await or .then.catch
-      projectRef.get().then(snapshot => {
+      console.log("project listener is added");
+      let projectRef = await db.collection("projects"); // TODO: Make all of this function async await or .then.catch
+      projectRef
+        .get()
+        .then(snapshot => {
           const tempProjects = projects;
           snapshot.forEach(doc => {
             const projectData = doc.data().newProject;
@@ -62,21 +64,21 @@ const Dashboard = () => {
           setProjects(tempProjects);
         })
         .catch(err => {
-          console.log('Error getting documents', err);
+          console.log("Error getting documents", err);
         });
     };
-  
+
     const removeProjectListener = () => {
       // console.log("project listener is removed");
       projectsRefFirebase.off();
     };
     addProjectListener();
     return () => removeProjectListener();
-  }, [])
-  
+  }, []);
+
   // useEffect(() => {
-    // addProjectListener();
-    // return () => removeProjectListener();
+  // addProjectListener();
+  // return () => removeProjectListener();
   // }, []); // --- mount \ unmount
 
   useEffect(() => {
@@ -100,26 +102,29 @@ const Dashboard = () => {
         androidDeveloper: item[10],
         dataEngineer: item[11],
         machineLearningEngineer: item[12],
-        teamMembers: [],
-
+        teamMembers: []
       };
-      console.log("newProject.title: ", newProject)
-      console.log("NewProject.description: ", newProject.description)
-      if (index > 0 && newProject.title !== '') {
-        db.collection('projects').add({newProject,})
+      console.log("newProject.title: ", newProject);
+      console.log("NewProject.description: ", newProject.description);
+      if (index > 0 && newProject.title !== "") {
+        db.collection("projects")
+          .add({ newProject })
           .then(ref => {
             // console.log('Added document with ID: ', ref.id);
-            console.log('ref:', ref)
+            console.log("ref:", ref);
             newProject.uid = ref.id;
-            console.log("ref.newProject: ", newProject)
-            console.log(Object.assign({}, { ...newProject }, {uid: ref.id}))
+            console.log("ref.newProject: ", newProject);
+            console.log(Object.assign({}, { ...newProject }, { uid: ref.id }));
             //ref.set(Object.assign({}, {...newProject}, {uid: ref.id}), {merge: true});
-            ref.set({newProject: {...ref.newProject, uid: ref.id}}, {merge: true})
-          })
+            ref.set(
+              { newProject: { ...ref.newProject, uid: ref.id } },
+              { merge: true }
+            );
+          });
       }
-      if(newProject.title !== '') {
-        console.log("NEWPROJECT.TITLE: ", newProject)
-        console.log("NEWPROJECT.DESCRIPTION: ", newProject.description)
+      if (newProject.title !== "") {
+        console.log("NEWPROJECT.TITLE: ", newProject);
+        console.log("NEWPROJECT.DESCRIPTION: ", newProject.description);
         return newProject;
       } else {
         return null;
@@ -146,12 +151,12 @@ const Dashboard = () => {
       distance: 100,
       maxPatternLength: 32,
       minMatchCharLength: 1,
-      keys: ['description', 'studentCohort', 'productType', 'title'],
+      keys: ["description", "studentCohort", "productType", "title"]
     };
     const fuse = new Fuse(list, options); // "list" is the item array
     const result = fuse.search(e.target.value);
-    if(e.target.value === '') {
-      setFilteredProj(projects)
+    if (e.target.value === "") {
+      setFilteredProj(projects);
     } else {
       setFilteredProj(result);
     }
@@ -170,7 +175,7 @@ const Dashboard = () => {
         focus
         icon="filter"
         iconPosition="left"
-        placeholder="Fuzzy Search Projects"
+        placeholder="Search by Project title, type or student name"
         type="text"
         onChange={e => fuzzySearch(projects, e)}
       />
@@ -178,20 +183,21 @@ const Dashboard = () => {
         {projects &&
           filteredProj &&
           filteredProj.map((item, index) => {
-            if(item) {
+            if (item) {
               //  console.log('From map', item);
-              let targetArr = item.productType.split(',');
-  
+              let targetArr = item.productType.split(",");
+
               return (
                 <div
                   key={index}
                   className="cardContainer"
-                  onClick={id => openProject(item.id)}>
+                  onClick={id => openProject(item.id)}
+                >
                   <Card raised={true} fluid={true} centered={true}>
                     <Card.Header className="cardHeader">
                       <h3 className="headerTitle">
                         {item.title.length > 25
-                          ? item.title.slice(0, 25) + '...'
+                          ? item.title.slice(0, 25) + "..."
                           : item.title}
                       </h3>
                     </Card.Header>
@@ -199,7 +205,7 @@ const Dashboard = () => {
                       <Card.Description className="description">
                         <p className="descriptionText">
                           {item.description.length > 200
-                            ? item.description.slice(0, 200) + '...'
+                            ? item.description.slice(0, 200) + "..."
                             : item.description}
                         </p>
                       </Card.Description>
@@ -223,7 +229,9 @@ const Dashboard = () => {
                   </Card>
                 </div>
               );
-            } else {return null}
+            } else {
+              return null;
+            }
           })}
       </div>
     </div>
@@ -236,17 +244,18 @@ const Dashboard = () => {
   // }
 
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{ textAlign: "center" }}>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: '#ba112e',
-          marginBottom: '40px',
-        }}>
-        <Header as="h1" inverted style={{marginTop: '25px'}}>
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "#ba112e",
+          marginBottom: "40px"
+        }}
+      >
+        <Header as="h1" inverted style={{ marginTop: "25px" }}>
           {/* <Icon color="white" name="chevron up"/> */}
-          <Icon name="chevron up" style={{color: "white"}}/>
+          <Icon name="chevron up" style={{ color: "white" }} />
           Lambda Group Organizer
         </Header>
         <Button
@@ -255,7 +264,8 @@ const Dashboard = () => {
           size="mini"
           // className="mini ui negative basic button logoutButton"
           onClick={signOut}
-          style={{marginLeft: '1%', alignSelf: 'center', color: 'white'}}>
+          style={{ marginLeft: "1%", alignSelf: "center", color: "white" }}
+        >
           <i className="icon sign-out" />
           Logout
         </Button>
--- src/components/admin/AdminDashboard/AddMinion.js
+++ src/components/admin/AdminDashboard/AddMinion.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import DisplayAllAdmins from '../AdminDashboard/DisplayAllAdmins/DisplayAllAdmins.js';
+import React, { useState, useEffect } from "react";
+import DisplayAllAdmins from "../AdminDashboard/DisplayAllAdmins/DisplayAllAdmins.js";
 //import firebase from '../../logic/firebase';
-import {db} from '../../../logic/firebase.js';
-import Fuse from 'fuse.js';
-import {Form, Button} from 'semantic-ui-react';
+import { db } from "../../../logic/firebase.js";
+import Fuse from "fuse.js";
+import { Form, Button } from "semantic-ui-react";
 
 const AddMinion = props => {
   const [students, setStudents] = useState([]);
@@ -11,16 +11,16 @@ const AddMinion = props => {
   const [triggerAdminFunc, setTriggerAdminFunc] = useState(false);
 
   const fetchStudents = async () => {
-    console.log('running filtered students');
+    console.log("running filtered students");
     let studentArr = [];
     await db
-      .collection('students')
+      .collection("students")
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
           const studentData = {
             name: doc.data().displayName,
-            email: doc.data().email,
+            email: doc.data().email
           };
           studentArr.push(studentData);
           console.log(studentData);
@@ -44,11 +44,11 @@ const AddMinion = props => {
       distance: 100,
       maxPatternLength: 32,
       minMatchCharLength: 1,
-      keys: ['name', 'email'],
+      keys: ["name", "email"]
     };
     const fuse = new Fuse(studentArr, options);
     const result = fuse.search(e.target.value);
-    if (e.target.value === '') {
+    if (e.target.value === "") {
       setFilteredStudents(students);
     } else {
       setFilteredStudents(result);
@@ -56,15 +56,15 @@ const AddMinion = props => {
   };
 
   const makeOverloard = (s, role) => {
-    db.collection('admin')
+    db.collection("admin")
       .doc(`${s.email}`)
       .set({
         displayName: s.name,
         email: s.email,
-        role: role,
+        role: role
       })
       .then(function() {
-        console.log('Granted Permissions!');
+        console.log("Granted Permissions!");
         setTriggerAdminFunc(!triggerAdminFunc);
       })
       .catch(err => {
@@ -84,7 +84,7 @@ const AddMinion = props => {
         focus
         icon="filter"
         iconPosition="left"
-        placeholder="Fuzzy Search Users"
+        placeholder="Search Users"
         type="text"
         onChange={e => searchStudents(students, e)}
       />
@@ -95,10 +95,10 @@ const AddMinion = props => {
               <p>
                 {s.name} | {s.email}
               </p>
-              <Button onClick={() => makeOverloard(s, 'overlord')}>
+              <Button onClick={() => makeOverloard(s, "overlord")}>
                 Make a Admin!
               </Button>
-              <Button onClick={() => makeOverloard(s, 'minion')}>
+              <Button onClick={() => makeOverloard(s, "minion")}>
                 Make a Smaller Admin!
               </Button>
             </div>
